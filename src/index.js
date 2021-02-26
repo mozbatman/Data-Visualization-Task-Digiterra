7a0707ccd3f21eb9f303bc9e817c4994317ed271
Mustafa Ozbatman <mozbatman@users.noreply.github.com>
2021-02-26 20:23:06 +0300

    Add title

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import "./styles/shared.scss";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
